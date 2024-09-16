64bc582af10b5c043eb0805a5ba7ba1128e5dfa0
Hephzibah Ofomi <h.ofomi@alustudent.com>
2024-09-16 12:56:28 +0200

    regex file

--- currency_regex.py
+++ currency_regex.py
@@ -1,5 +1,5 @@
 import re
 
 def extract_currency(text):
-    currency_pattern = r'\$\d{1,3}(,\d{3})*(\.\d{2})?'
+    currency_pattern = r'\$\d{1,3}(?:,\d{3})*(?:\.\d{2})?'
     return re.findall(currency_pattern, text)
