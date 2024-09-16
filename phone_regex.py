03b561212d2e4aa45a8d68baa064f46a2e08c57d
root <o.attah@alustudent.com>
2024-09-16 10:20:59 +0000

    phone_regex.py

--- phone_regex.py
+++ phone_regex.py
@@ -1,5 +1,5 @@
 import re
 
 def extract_phone_numbers(text):
-    phone_pattern = r'\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}'
+    phone_pattern = r'\(?\d{3}\)?[.\-\s]?\d{3}[.\-\s]?\d{4}'
     return re.findall(phone_pattern, text)
