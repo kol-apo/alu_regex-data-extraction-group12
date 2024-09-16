b9dd418269431dcbdc8aec8b906728fc4cf260a4
Hephzibah Ofomi <h.ofomi@alustudent.com>
2024-09-16 13:19:55 +0200

    regex file

--- time_regex.py
+++ time_regex.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
 import re
 
-import re
-
 def extract_times(text):
-    # Define 24-hour time format
-    time_pattern_24 = r'([01]?\d|2[0-3]):[0-5]\d'
-    # Define 12-hour time format (with optional AM/PM)
-    time_pattern_12 = r'([01]?\d):[0-5]\d\s?[APap][Mm]'
+    time_pattern_24 = r'\b([01]?[0-9]|2[0-3]):[0-5][0-9]\b'
+    time_pattern_12 = r'\b(1[0-2]|0?[1-9]):[0-5][0-9]\s?(AM|PM|am|pm)\b'
 
-    # Combine both patterns
     combined_pattern = time_pattern_24 + '|' + time_pattern_12
-
-    # Extract times using the combined pattern
     return re.findall(combined_pattern, text)
