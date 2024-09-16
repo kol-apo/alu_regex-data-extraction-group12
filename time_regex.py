9436b335b7d6fed4f2e56e9a82269d5ea05bc676
Hephzibah Ofomi <h.ofomi@alustudent.com>
2024-09-16 12:58:03 +0200

    regex file

--- time_regex.py
+++ time_regex.py
@@ -2,8 +2,6 @@
 import re
 
 def extract_times(text):
-    time_pattern_24 = r'\b([01]?[0-9]|2[0-3]):[0-5][0-9]\b'
-    time_pattern_12 = r'\b(1[0-2]|0?[1-9]):[0-5][0-9]\s?(AM|PM|am|pm)\b'
-
+    time_pattern = r'([01]?\d|2[0-3]):[0-5]\d(?:\s?[APap][Mm])?'
     combined_pattern = time_pattern_24 + '|' + time_pattern_12
     return re.findall(combined_pattern, text)
