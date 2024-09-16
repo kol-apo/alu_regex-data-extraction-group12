e287dff33a4defaf2f604e1b3e92adab268d90b9
root <o.attah@alustudent.com>
2024-09-16 10:04:55 +0000

    time_regex.py

--- time_regex.py
+++ time_regex.py
@@ -2,7 +2,8 @@
 import re
 
 def extract_times(text):
-    time_pattern_24 = r'\b([01]?[0-9]|2[0-3]):[0-5][0-9]\b'  # 24-hour format
-    time_pattern_12 = r'\b(1[0-2]|0?[1-9]):[0-5][0-9]\s?(AM|PM|am|pm)\b'  # 12-hour format
-    return re.findall(f'{time_pattern_24}|{time_pattern_12}', text)
+    time_pattern_24 = r'\b([01]?[0-9]|2[0-3]):[0-5][0-9]\b'
+    time_pattern_12 = r'\b(1[0-2]|0?[1-9]):[0-5][0-9]\s?(AM|PM|am|pm)\b'
 
+    combined_pattern = f'{time_pattern_24}|{time_pattern_12}'
+    return re.findall(combined_pattern, text)
